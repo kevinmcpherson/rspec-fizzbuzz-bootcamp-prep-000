97f5ce154cd41472ccb9f4c2bf55c296e73070cd
Kevin McPherson <kjmcpherson@gmail.com>
2018-01-30 17:45:57 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,9 @@
 def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0
-    "FizzBuzz"
-  end
-end
-
-def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
   else int % 5 == 0
     "Buzz"
+  elsif if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"  
   end
 end
